bfc4142b08f32d281539bce8d357e2db434c5f15
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-02-24 23:07:52 -0800

    adding styles and creating simple courseGoal state

--- App.js
+++ App.js
@@ -1,33 +1,62 @@
-// import { useState } from "expo-status-bar";
 import React, { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  // used as a helper function to setCourseGoals to be the spreaded currentGoals WITH  the newly entered goals
+  const addGoalHandler = () => {
+    // setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder="Course Goal" style={styles.textInput} />
-        <Button title="ADD" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-
-      <View></View>
+      <View>
+        {courseGoals.map((goal, i) => (
+          <View style={styles.listItem}>
+            <Text key={i}> {goal} </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
   container: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   textInput: {
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
+    width: "80%",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
